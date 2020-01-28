67681bdc1bae8269d5873681e208a8e16142dd1d
SyntheticAutomation <syntheticauto900@gmail.com>
2020-01-28 07:50:33 -0700

    Adds projects section with css and jquery animations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,51 @@
 //= require_tree .
 
 $(document).ready(function () {
+  $(document).on('mouseover', '.box1', function(){
+    $('.projects-container').addClass('bg1').removeClass('bg2 bg3 bg4');
+    $('.box1 .box').addClass('active');
+    $('.box1 h1').addClass('highlight');
+    $('.box2 h1').removeClass('highlight');
+    $('.box3 h1').removeClass('highlight');
+    $('.box4 h1').removeClass('highlight');
+    $('.box2 .box').removeClass('active');
+    $('.box3 .box').removeClass('active');
+    $('.box4 .box').removeClass('active');
+  });
+  $(document).on('mouseover', '.box2', function(){
+    $('.projects-container').addClass('bg2').removeClass('bg1 bg3 bg4');
+    $('.box2 .box').addClass('active');
+    $('.box2 h1').addClass('highlight');
+    $('.box1 h1').removeClass('highlight');
+    $('.box3 h1').removeClass('highlight');
+    $('.box4 h1').removeClass('highlight');
+    $('.box1 .box').removeClass('active');
+    $('.box3 .box').removeClass('active');
+    $('.box4 .box').removeClass('active');
+  });
+  $(document).on('mouseover', '.box3', function(){
+    $('.projects-container').addClass('bg3').removeClass('bg1 bg2 bg4');
+    $('.box3 .box').addClass('active');
+    $('.box3 h1').addClass('highlight');
+    $('.box1 h1').removeClass('highlight');
+    $('.box2 h1').removeClass('highlight');
+    $('.box4 h1').removeClass('highlight');
+    $('.box1 .box').removeClass('active');
+    $('.box2 .box').removeClass('active');
+    $('.box4 .box').removeClass('active');
+  });
+  $(document).on('mouseover', '.box4', function(){
+    $('.projects-container').addClass('bg4').removeClass('bg1 bg2 bg3');
+    $('.box4 .box').addClass('active');
+    $('.box4 h1').addClass('highlight');
+    $('.box1 h1').removeClass('highlight');
+    $('.box2 h1').removeClass('highlight');
+    $('.box3 h1').removeClass('highlight');
+    $('.box1 .box').removeClass('active');
+    $('.box2 .box').removeClass('active');
+    $('.box3 .box').removeClass('active');
+  });
+  
   // ------------------found the svg logo path length for each letter with this ------------------
   // const logo = document.querySelectorAll("#name-logo path");
   // console.log(logo);
@@ -24,5 +69,9 @@ $(document).ready(function () {
   //   console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
   // }
   // ------------------------------------------------------------------------------------------
+
 });
   
+
+
+
